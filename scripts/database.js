c21ffa2052e91f70b2b2e98c6cec662a76363fd4
Amanda Wright <akhawkins1@hotmail.com>
2023-02-24 10:30:07 -0500

    added what they eat

---
+++ scripts/database.js
@@ -0,0 +1,31 @@
+const database = {
+    fish: [
+        {
+            name: "Bart",
+            species:
+            size: 3 inches
+            quotes:
+            locations:
+            food: "crustaceans",
+            
+        },
+        {next fish keys and values
+        
+            name: "Bart",
+            species:
+            size: 3 inches
+            quotes:
+            locations:
+            food: "crustaceans",
+        }
+        {
+
+        name: "Bart",
+        species:
+        size: 3 inches
+        quotes:
+        locations:
+        food: "crustaceans",
+        }
+    ]
+}
